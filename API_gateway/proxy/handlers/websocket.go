6dc92cb1a7f8c8074d562948951ef6599df72c2e
Andrew-Pohl <55916961+Andrew-Pohl@users.noreply.github.com>
2024-09-06 17:04:33 +0100

    Add query string update websocket handling

--- API_gateway/proxy/handlers/websocket.go
+++ API_gateway/proxy/handlers/websocket.go
@@ -1,61 +1,63 @@
 package handlers
 
 import (
-	"log"
-	"sync"
-	"time"
+        "log"
+        "sync"
+        "time"
 
-	"github.com/fasthttp/websocket"
-	"github.com/valyala/fasthttp"
+        "github.com/fasthttp/websocket"
+        "github.com/valyala/fasthttp"
 
-	"proxy/proxy"
+        "proxy/proxy"
 )
 
 func handleWebSocketRequest(ctx *fasthttp.RequestCtx, apiKey string, chainMap map[string][]string, keyData map[string]interface{}) {
-	upgrader := websocket.FastHTTPUpgrader{
-		ReadBufferSize:  4096,
-		WriteBufferSize: 4096,
-		CheckOrigin: func(ctx *fasthttp.RequestCtx) bool {
-			return true
-		},
-	}
+        upgrader := websocket.FastHTTPUpgrader{
+                ReadBufferSize:  8192,
+                WriteBufferSize: 8192,
+                CheckOrigin: func(ctx *fasthttp.RequestCtx) bool {
+                        return true
+                },
+        }
 
-	err := upgrader.Upgrade(ctx, func(conn *websocket.Conn) {
-		defer conn.Close()
+        err := upgrader.Upgrade(ctx, func(conn *websocket.Conn) {
+                defer conn.Close()
 
-		// Set a read deadline to prevent hanging connections (optional)
-		conn.SetReadDeadline(time.Now().Add(10 * time.Minute))
+                // Set a read deadline to prevent hanging connections (optional)
+                conn.SetReadDeadline(time.Now().Add(60 * time.Minute))
 
-		if chainCode, ok := chainMap[keyData["chain"].(string)]; ok {
-			if len(chainCode) != 0 {
-				//uri := chainCode[0] + path
+                if chainCode, ok := chainMap[keyData["chain"].(string)]; ok {
+                        if len(chainCode) != 0 {
+                                //uri := chainCode[0] + path
 
-				backendURL := chainCode[0]
-				backendConn, _, err := websocket.DefaultDialer.Dial(backendURL, nil)
-				if err != nil {
-					log.Printf("Failed to connect to backend: %s", err)
-					return
-				}
-				defer backendConn.Close()
-				// Start proxying messages between the client and backend
-				var wg sync.WaitGroup
-				wg.Add(2)
+                                backendURL := chainCode[0]
+                                backendConn, _, err := websocket.DefaultDialer.Dial(backendURL, nil)
+                                if err != nil {
+                                        log.Printf("Failed to connect to backend: %s", err)
+                                        return
+                                }
+                                defer backendConn.Close()
+                                // Start proxying messages between the client and backend
+                                var wg sync.WaitGroup
+                                wg.Add(2)
 
-				go func() {
-					defer wg.Done()
-					proxy.ProxyWebSocketMessages(conn, backendConn, apiKey, keyData)
-				}()
-				go func() {
-					defer wg.Done()
-					proxy.ProxyWebSocketMessages(backendConn, conn, apiKey, keyData)
-				}()
+                                go func() {
+                                        defer wg.Done()
+                                        proxy.ProxyWebSocketMessages(conn, backendConn, apiKey, keyData)
+                                        conn.Close()
+                                }()
+                                go func() {
+                                        defer wg.Done()
+                                        proxy.ProxyWebSocketMessages(backendConn, conn, apiKey, keyData)
+                                        backendConn.Close()
+                                }()
 
-				wg.Wait()
-			}
-		}
-	})
+                                wg.Wait()
+                        }
+                }
+        })
 
-	if err != nil {
-		log.Printf("WebSocket upgrade error: %v", err)
-	}
+        if err != nil {
+                log.Printf("WebSocket upgrade error: %v", err)
+        }
 }
--- API_gateway/proxy/utils/helpers.go
+++ API_gateway/proxy/utils/helpers.go
@@ -1,45 +1,66 @@
 package utils
 
 import (
-	"fmt"
-	"log"
-	"strings"
-
-	"github.com/valyala/fasthttp"
+        "net/url"
+        "log"
+        "strings"
+        "github.com/valyala/fasthttp"
 )
 
 // ExtractAPIKeyAndPath extracts API key and path from request context
 func ExtractAPIKeyAndPath(ctx *fasthttp.RequestCtx) (string, string, error) {
-	apiKey := string(ctx.Request.Header.Peek("x-api-key"))
-	path := string(ctx.Path())
-	if apiKey == "" || path == "" {
-		return "", "", fmt.Errorf("API key or path is missing")
-	}
-	return apiKey, path, nil
+uri := string(ctx.RequestURI())
+    parsedURI, err := url.Parse(uri)
+    if err != nil {
+        return "", "", err
+    }
+    path := parsedURI.Path
+    apiKey := extractAPIKey(path)
+    return apiKey, path, nil
+}
+
+// extractAPIKey extracts API key from the query string
+func extractAPIKey(queryString string) string {
+    parts := strings.Split(queryString, "=")
+    if len(parts) < 2 || parts[0] != "/api" {
+        return ""
+    }
+
+    keys := strings.Split(parts[1], "/")
+    return keys[0]
 }
 
 // ExtractAdditionalPath extracts additional path information
-func ExtractAdditionalPath(path string) string {
-	parts := strings.Split(path, "/")
-	if len(parts) > 2 {
-		// parts[0] will be an empty string because the string starts with '/'
-		remainingParts := parts[2:]
-		reconstructedPath := "/" + strings.Join(remainingParts, "/")
+func ExtractAdditionalPath(path string, queryString string) string {
+        parts := strings.Split(path, "/")
+        if len(parts) > 2 {
+                // parts[0] will be an empty string because the string starts with '/'
+                remainingParts := parts[2:]
+                reconstructedPath := "/" + strings.Join(remainingParts, "/")
 
-		return reconstructedPath
-	} else {
-		return ""
-	}
+                if queryString != "" {
+                        decodedQueryString, err := url.QueryUnescape(queryString)
+                        if err != nil {
+                                return ""
+                        }
+                        reconstructedPath = reconstructedPath + "?" + decodedQueryString
+                }
+
+                return reconstructedPath
+        } else {
+                return ""
+        }
 }
+
 
 
 // isWebSocketRequest checks if the request is a WebSocket upgrade request
 func IsWebSocketRequest(ctx *fasthttp.RequestCtx) bool {
-	return string(ctx.Request.Header.Peek("Upgrade")) == "websocket"
+        return string(ctx.Request.Header.Peek("Upgrade")) == "websocket"
 }
 
 // HandleProxyError handles errors during proxy requests
 func HandleProxyError(ctx *fasthttp.RequestCtx, err error) {
-	log.Printf("Error proxying request: %s", err)
-	ctx.Error("Error proxying request", fasthttp.StatusInternalServerError)
+        log.Printf("Error proxying request: %s", err)
+        ctx.Error("Error proxying request", fasthttp.StatusInternalServerError)
 }
